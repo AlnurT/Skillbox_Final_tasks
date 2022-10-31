a92538153b3b934fe850b2eac0fd0a478984d3cd
AlnurT <alnur.tazhbenov@mail.ru>
2022-10-31 15:53:12 +0300

    Task 28.3: Pyramid and Triangle

--- Deep_class_work_and_built-in_decorators/Task_28_3/task_28_3.py
+++ Deep_class_work_and_built-in_decorators/Task_28_3/task_28_3.py
@@ -13,54 +13,65 @@
 Используя входные данные о фигурах и знания математики, реализуйте соответствующие
 классы и методы. Для базовых классов также реализуйте геттеры и сеттеры.
 """
-"""from math import sqrt"""
+from math import sqrt
 
 
 class Square:
-    def __init__(self, side: int) -> None:
-        self.__side = side
+    def __init__(self, square_side: int) -> None:
+        self.__square_side = square_side
 
     @property
     def square_side(self) -> int:
         return self.square_side
 
     @square_side.setter
-    def square_side(self, side: int) -> None:
-        self.__side = side
+    def square_side(self, square_side: int) -> None:
+        self.__square_side = square_side
 
     @property
     def square_surface_area(self) -> int:
-        return self.__side**2
+        return self.__square_side**2
 
     @property
     def square_perimeter(self) -> int:
-        return self.__side * 4
+        return self.__square_side * 4
 
 
-"""class Triangle:
-    @property
-    def surface_area(self) -> int:
-        return self.surface_area
-
-    @surface_area.setter
-    def surface_area(self, base_height: tuple) -> None:
-        base, height = base_height
-        self.surface_area = base * height / 2
+class Triangle:
+    def __init__(self, base: int, triangle_height: int) -> None:
+        self.__base = base
+        self.__triangle_height = triangle_height
 
     @property
-    def perimeter(self) -> int:
-        return self.perimeter
+    def base(self) -> int:
+        return self.__base
 
-    @perimeter.setter
-    def perimeter(self, base_height: tuple) -> None:
-        side = sqrt(self.__height ** 2 + (self.__base / 2) ** 2)
-        self.perimeter = self.__base + side * 2"""
+    @base.setter
+    def base(self, base: int) -> None:
+        self.__base = base
+
+    @property
+    def triangle_height(self) -> int:
+        return self.__triangle_height
+
+    @triangle_height.setter
+    def triangle_height(self, triangle_height: int) -> None:
+        self.__triangle_height = triangle_height
+
+    @property
+    def triangle_surface_area(self) -> int:
+        return round(self.__base * self.__triangle_height / 2)
+
+    @property
+    def triangle_perimeter(self) -> int:
+        triangle_side = sqrt(self.__triangle_height**2 + (self.__base / 2) ** 2)
+        return round(self.__base + triangle_side * 2)
 
 
 class Cube(Square):
-    def __init__(self, side: int) -> None:
-        super().__init__(side)
-        self.__surfaces_list = [Square(side=side) for _ in range(6)]
+    def __init__(self, cube_side: int) -> None:
+        super().__init__(square_side=cube_side)
+        self.__surfaces_list = [Square(square_side=cube_side) for _ in range(6)]
         self.__cube_surface_area = 0
 
     @property
@@ -70,30 +81,44 @@ class Cube(Square):
         return self.__cube_surface_area
 
 
-"""class Pyramid(Square, Triangle):
-    def __init__(self, base: int, height: int) -> None:
-        self.__base = base
-        self.__height = height
-        self.__surfaces_list = [Triangle for _ in range(4)]
-        self.__cube_surface_area = 0
+class Pyramid(Square, Triangle):
+    def __init__(self, pyramid_base: int, pyramid_height: int) -> None:
+        Square.__init__(self, square_side=pyramid_base)
+        self.triangle_height = round(
+            sqrt(pyramid_height**2 + (pyramid_base / 2) ** 2)
+        )
+        Triangle.__init__(self, base=pyramid_base, triangle_height=self.triangle_height)
+        self.__pyramid_height = pyramid_height
+        self.__surfaces_list = [
+            Triangle(base=pyramid_base, triangle_height=self.triangle_height)
+            if surface < 4
+            else Square(square_side=pyramid_base)
+            for surface in range(5)
+        ]
+        self.__pyramid_surface_area = 0
 
-    def calculate_cube_area(self) -> int:
-        for _ in self.__surfaces_list:
-            self.__cube_surface_area += self.calculate_square_area()
-        return self.__cube_surface_area"""
+    @property
+    def pyramid_surface_area(self) -> int:
+        for surface in self.__surfaces_list:
+            if isinstance(surface, Triangle):
+                self.__pyramid_surface_area += surface.triangle_surface_area
+            elif isinstance(surface, Square):
+                self.__pyramid_surface_area += surface.square_surface_area
+        return self.__pyramid_surface_area
 
 
 def main():
-    cube = Cube(side=5)
+    cube = Cube(cube_side=5)
     print(f"Площадь поверхности куба: {cube.cube_surface_area}")
     print(f"Площадь грани: {cube.square_surface_area}")
-    print(f"Периметр грани: {cube.square_perimeter}")
+    print(f"Периметр грани: {cube.square_perimeter}\n")
 
-
-"""    pyramid = Pyramid(base=4, height=5)
-    print(f"Площадь поверхности куба: {cube.calculate_cube_area}")
-    print(f"Площадь грани: {cube.surface_area}")
-    print(f"Периметр грани: {cube.perimeter}")"""
+    pyramid = Pyramid(pyramid_base=6, pyramid_height=4)
+    print(f"Площадь поверхности пирамиды: {pyramid.pyramid_surface_area}")
+    print(f"Площадь боковой поверхности: {pyramid.triangle_surface_area}")
+    print(f"Периметр боковой поверхности: {pyramid.triangle_perimeter}")
+    print(f"Площадь основания: {pyramid.square_surface_area}")
+    print(f"Периметр основания: {pyramid.square_perimeter}")
 
 
 if __name__ == "__main__":
