d8a752840aaf4c82593918ad4a240ef64108668a
AlnurT <alnur.tazhbenov@mail.ru>
2022-11-04 13:18:48 +0300

    Task 29.4: The whole world is a decorator

---
+++ Decorators - Advanced/Task_29_4/task_29_4.py
@@ -0,0 +1,54 @@
+"""Задача 4. Весь мир — декоратор…
+
+Реализуйте декоратор для декораторов: он должен декорировать другую функцию,
+которая должна быть декоратором, и даёт возможность любому декоратору принимать
+произвольные аргументы.
+
+Пример использования:
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func: Callable, *args, **kwargs):... # отсюда уже сами!
+
+
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет", text, num)
+
+
+decorated_function("Юзер", 101)
+
+Результат:
+Переданные арги и кварги в декоратор: (100, 'рублей', 200, 'друзей') {}
+Привет, Юзер 101
+"""
+from typing import Callable
+
+
+def decorator_with_args_for_any_decorator(func: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        print(f"Переданные арги и кварги в декоратор: {args} {kwargs}")
+        return func
+
+    return wrapper
+
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        return func
+
+    return wrapper
+
+
+@decorated_decorator(100, "рублей", 200, "друзей")
+def decorated_function(text: str, num: int) -> None:
+    print("Привет,", text, num)
+
+
+def main() -> None:
+    decorated_function("Юзер", 101)
+
+
+if __name__ == "__main__":
+    main()
