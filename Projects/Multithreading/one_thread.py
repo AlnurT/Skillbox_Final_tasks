3b0e13b7e3d0e07f6b6558e0ebac6d39496dd211
AlnurT <alnur.tazhbenov@mail.ru>
2022-11-23 23:53:18 +0300

    Multi thread

--- Projects/Multithreading/one_thread.py
+++ Projects/Multithreading/one_thread.py
@@ -8,49 +8,40 @@
 - подсчитать общий размер этих файлов
 - вывести результат на консоль
 """
-import time
-from collections import defaultdict
-from typing import Callable
+from threading import Thread
 from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
+from Multithreading.utils import timer
 
 
-class PageSizer:
-    def __init__(self, sites: list) -> None:
-        self.sites: list = sites
-        self.url_size: dict = defaultdict()
+class PageSizer(Thread):
+    def __init__(self, url: str, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.url_base: str = url
+        self.size: int = 0
 
-    def find_tag(self, url_base: str):
-        html_data = self.count_bytes(url=url_base)
+    def run(self) -> None:
+        html_data = self._count_bytes(url=self.url_base)
         soup = BeautifulSoup(html_data, "lxml")
 
         for link in soup.find_all(name=["link", "script"]):
             if link.get("rel") == "stylesheet":
-                extra_url = urljoin(url_base, link.get("href"))
+                extra_url = urljoin(self.url_base, link.get("href"))
             elif link.get("src"):
-                extra_url = urljoin(url_base, link.get("src"))
+                extra_url = urljoin(self.url_base, link.get("src"))
             else:
                 continue
-            self.count_bytes(url=extra_url)
+            self._count_bytes(url=extra_url)
 
-    def count_bytes(self, url: str) -> str:
+    def _count_bytes(self, url: str) -> str:
         url_req = requests.get(url)
         html_data = url_req.text
-        self.url_size[url] = len(html_data)
+        size = len(html_data)
+        self.size += size
+        print(f"{url:<115} {size // 1024:<4} Kb ({size:<7} b)")
         return html_data
-
-
-def timer(func: Callable) -> Callable:
-    def wrapped_func(*args, **kwargs):
-        start = time.time()
-        func(*args, **kwargs)
-        func_time = round(time.time() - start, 2)
-        print(f"Время выполнения операции: {func_time} c")
-        return func
-
-    return wrapped_func
 
 
 @timer
@@ -62,17 +53,17 @@ def main() -> None:
         "https://work-zilla.com",
         "https://iklife.ru",
     ]
-    sizer = PageSizer(sites=sites)
-
-    for url in sites:
-        sizer.find_tag(url_base=url)
+    sizers = [PageSizer(url=url) for url in sites]
 
     print(f"Сайт:{' ' * 110} Размер:")
     print("-" * 135)
-    for url, size in sizer.url_size.items():
-        print(f"{url:<115} {size // 1024:<4} Kb ({size:<7} b)")
 
-    total_bytes = sum(sizer.url_size.values()) // 1024
+    for sizer in sizers:
+        sizer.start()
+    for sizer in sizers:
+        sizer.join()
+
+    total_bytes = sum(sizer.size for sizer in sizers) // 1024
     print(f"\nОбщий размер: {total_bytes} Kb")
 
 
