39fc7cafe5c97fd3ead9ab67c3d0d5b2959b66a8
AlnurT <alnur.tazhbenov@mail.ru>
2022-12-09 14:22:05 +0300

    Saving photos

--- Projects/working_with_resources/task.py
+++ Projects/working_with_resources/task.py
@@ -6,25 +6,45 @@
 - Обрезать фото до портрета
 - Сохранить в базу данных
 """
+import os
 import re
 
 import requests
 from bs4 import BeautifulSoup
 
 
-def save_all_photos(url: str) -> None:
+def save_file(file_url: str) -> None:
+    """
+    Сохранение каждой фото в папку
+
+    :param file_url: неполная ссылка на фото
+    """
+
+    filename_match = re.match(r".*/(\w+).*\.(\w+)", file_url)
+    if filename_match and filename_match.group(1) != "def_avatar":
+        file_name = ".".join(filename_match.groups())
+        file_path = os.path.join(os.getcwd(), "photos", file_name)
+        with open(file_path, "wb") as file:
+            file.write(requests.get(file_url).content)
+
+
+def find_url_with_img(url: str) -> None:
+    """
+    Сохранение всех фото в папку
+
+    Функция ищет все фотографии по ссылке и сохраняет каждую в папку со своим именем
+    :param url: ссылка на сайт для скачивания фотографий
+    """
     html_doc = requests.get(url)
     soup = BeautifulSoup(html_doc.text, "html.parser")
     for link in soup.find_all("img"):
-        file_path = link.get("src")
-        filename_match = re.match(r".*/(\w+).*\.(\w+)", file_path)
-        if filename_match:
-            filename = ".".join(filename_match.groups())
-            print(filename)
+        file_url = f"https://spbau.ru/{link.get('src')}"
+        if file_url:
+            save_file(file_url)
 
 
 def main() -> None:
-    save_all_photos(url="https://spbau.ru/kontaktyi/sotrudniki")
+    find_url_with_img(url="https://spbau.ru/kontaktyi/sotrudniki")
 
 
 if __name__ == "__main__":
