26a8ea91cb63874cf67346f69233ce1d7a93bd02
AlnurT <alnur.tazhbenov@mail.ru>
2022-12-12 17:08:12 +0300

    Database

--- Projects/working_with_resources/task.py
+++ Projects/working_with_resources/task.py
@@ -3,49 +3,58 @@
 
 Задачи:
 - Сохранить фото людей в папку
-- Обрезать фото до портрета
 - Сохранить в базу данных
 """
 import os
 import re
 
+import peewee
 import requests
 from bs4 import BeautifulSoup
 from working_with_resources.utils import timer
 
 
-def save_file(file_url: str) -> None:
+class Photos(peewee.Model):
     """
-    Сохранение каждой фото в папку
+    Класс таблицы базы данных
+    """
+
+    name = peewee.CharField()
+    path = peewee.CharField()
+    url = peewee.CharField()
+
+    class Meta:
+        database = peewee.SqliteDatabase("photo.db")
+
+
+def save_photo_add_in_database(file_url: str) -> None:
+    """
+    Сохранение каждой фото в папку и базу данных
 
     :param file_url: неполная ссылка на фото
     """
+
     filename_match = re.match(r".*/(\w+).*\.(\w+)", file_url)
-    if filename_match and filename_match.group(1) != "def_avatar":
-        file_name = ".".join(filename_match.groups())
-        file_path = os.path.join(os.getcwd(), "photos", file_name)
-        with open(file_path, "wb") as file:
-            file.write(requests.get(file_url).content)
-
-
-def find_url_with_img(url: str) -> None:
-    """
-    Сохранение всех фото в папку
-
-    Функция ищет все фотографии по ссылке и сохраняет каждую в папку со своим именем
-    :param url: ссылка на сайт для скачивания фотографий
-    """
-    html_doc = requests.get(url)
-    soup = BeautifulSoup(html_doc.text, "html.parser")
-    for link in soup.find_all("img"):
-        file_url = f"https://spbau.ru/{link.get('src')}"
-        if file_url:
-            save_file(file_url)
+    if filename_match:
+        if filename_match.group(1) != "def_avatar" and filename_match.group(2) == "jpg":
+            file_name = ".".join(filename_match.groups())
+            img_path = os.path.join(os.getcwd(), "photos", file_name)
+            with open(img_path, "wb") as file:
+                img = requests.get(file_url).content
+                file.write(img)
+            Photos.create(name=file_name, path=img_path, url=file_url)
 
 
 @timer
 def main() -> None:
-    find_url_with_img(url="https://spbau.ru/kontaktyi/sotrudniki")
+    url = "https://spbau.ru/kontaktyi/sotrudniki"
+    html_doc = requests.get(url)
+    soup = BeautifulSoup(html_doc.text, "html.parser")
+    Photos.create_table()
+    for link in soup.find_all("img"):
+        file_url = f"https://spbau.ru/{link.get('src')}"
+        if file_url:
+            save_photo_add_in_database(file_url=file_url)
 
 
 if __name__ == "__main__":
